350b161c9ec0532d7c6aef0a65a24356c19d5ac9
superakura <superakura@hotmail.com>
2018-07-30 15:22:59 +0800

    初步完成招标进度界面显示

---
+++ TenderInfo/Controllers/ProgressInfoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TenderInfo.Controllers
+{
+    public class ProgressInfoController : Controller
+    {
+        //ProgressInfo
+
+        public ViewResult ProgressMaterial()
+        {
+            return View();
+        }
+
+        public ViewResult ProgressProject()
+        {
+            return View();
+        }
+
+        public ViewResult ProgressFrame()
+        {
+            return View();
+        }
+    }
+}
